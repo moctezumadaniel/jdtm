63cccebc4926c389dded52334abccc6358037ad8
moctezumadaniel <moctezumadanielt@gmail.com>
2021-08-08 16:10:32 -0500

    center curriculum items

--- src/components/Curriculum/index.js
+++ src/components/Curriculum/index.js
@@ -1,31 +1,35 @@
+import Button  from 'react-bootstrap/Button'
 import Accordion from 'react-bootstrap/Accordion'
 import styles from './Curriculum.module.css'
 function Curriculum (){
     return (
         <div className={styles.CurriculumContainer}>
-            <h1>Curriculum</h1>
-            <Accordion defaultActiveKey='0'>
-                <Accordion.Item>
-                    <Accordion.Header>
-                        <h4>Perfil</h4>
-                    </Accordion.Header>
-                    <Accordion.Body>
-                        Mi descripcion sobre mis deseos de convertirme en un 
-                        desarrollo front end
-                    </Accordion.Body>
-                </Accordion.Item>
-            </Accordion>
+            <div className={styles.CurriculumItemsContainer}>
+                <h1>Currículum</h1>
+                <Button>Descargar PDF</Button>
+                <Accordion defaultActiveKey='0'>
+                    <Accordion.Item>
+                        <Accordion.Header>
+                            <h4>Perfil</h4>
+                        </Accordion.Header>
+                        <Accordion.Body>
+                            Mi descripcion sobre mis deseos de convertirme en un 
+                            desarrollo front end
+                        </Accordion.Body>
+                    </Accordion.Item>
+                </Accordion>
 
-            <Accordion defaultActiveKey='0'>
-                <Accordion.Item>
-                    <Accordion.Header>
-                        <h4>Educación</h4>
-                    </Accordion.Header>
-                    <Accordion.Body>
-                        Mi descripcion sobre las tecnologias aprendidas
-                    </Accordion.Body>
-                </Accordion.Item>
-            </Accordion>
+                <Accordion defaultActiveKey='0'>
+                    <Accordion.Item>
+                        <Accordion.Header>
+                            <h4>Educación</h4>
+                        </Accordion.Header>
+                        <Accordion.Body>
+                            Mi descripcion sobre las tecnologias aprendidas
+                        </Accordion.Body>
+                    </Accordion.Item>
+                </Accordion>
+            </div>
         </div>
     )
 }
